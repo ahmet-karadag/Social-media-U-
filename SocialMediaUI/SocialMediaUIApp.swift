93409f4ff7bce41b9c680d09c8d748549ef78d44
Ahmet Karadağ <akkaradaga@gmail.com>
2026-07-23 22:16:41 +0300

    Initial Commit

---
+++ SocialMediaUI/SocialMediaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaUIApp.swift
+//  SocialMediaUI
+//
+//  Created by ahmet karadağ on 23.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct SocialMediaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
